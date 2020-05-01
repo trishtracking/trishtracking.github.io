7ee76e439de47bff436becc2fdcfc4587c8d87c8
Trish Wynne <trishasone@gmail.com>
2020-05-01 17:50:55 +0100

    add png versions of images, also fix bugs in left right codes and now it works I AM A GOD

--- image_carousel.js
+++ image_carousel.js
@@ -1,23 +1,36 @@
 
 
-var albumIndex,albums,albumName; 
+var albumIndex;
+var albums; 
+var albumName; 
+
 function initAlbums(){
-	albumIndex = 0;
-	albums=document.getElementsByClassName("albumCovers");
-	albums[albumIndex].style.opacity= 1; 
+	albumIndex=0;
+	albums=document.getElementsByClassName("albumCovers"); //array of all the album images with the class .albumCovers
+	albums[albumIndex].style.opacity= 0; //initiates the album 
+	albumName=document.querySelector(".albumNameHolder.albumName");
+	albumName.innerText=albums[albumIndex].querySelector(".albumName").innerText; 
 
-	albumName = document.querySelector(".albumNameHolder .albumName");
-	albumName.innerText = albums[albumIndex].querySelector(".albumName").innerText; 
 
- //disable nextPrevBtn if first album 
- if(albums.length<2) {
-	var nextPrevBtns=document.querySelector(".leftArrow,.rightArrow");
-	nextPrevBtns.style.display="none";
+ 	if(albums.length<2){
+		var nextPrevBtns=document.querySelector(".leftArrow,.rightArrow");
+		nextPrevBtns.style.display="none";
 	for (i = 0; i < nextPrevBtn.length; i++) {
 		nextPrevBtn[i].style.display="none";
+		}
 	}
-}
 
+	dots=[];
+    var dotsContainer=document.getElementById("dotsContainer"),i;
+    for (i = 0; i < albums.length; i++) {
+        var dot=document.createElement("span");
+        dot.classList.add("dots");
+        dotsContainer.append(dot);
+        dot.setAttribute("onclick","moveAlbum("+i+")");
+        dots.push(dot);
+    }
+    dots[albumIndex].classList.add("active");
+}
 initAlbums(); 
 function plusAlbums(n) {
 	moveAlbum(albumIndex+n);
@@ -25,21 +38,24 @@ function plusAlbums(n) {
 }
 function moveAlbum(n) {
 	var i;
-	var current,next; 
-	var moveAlbumAnimClass = {
+	var current;
+	var next; 
+	var moveAlbumAnimClass={
 		forCurent:"",
 		forNext:""
 	};
 	
-	// var AlbumTextAnimClass;
+	var albumNameAnimClass;
 	if(n>albumIndex) {
 		if(n >= albums.length) {n=0;}
 		moveAlbumAnimClass.forCurrent = "moveLeftCurrentAlbum";
 		moveAlbumAnimClass.forNext = "moveLeftNextAlbum";
+		albumNameAnimClass="albumNameFromTop";
 		} else if (n<albumIndex) {
 			if(n<0) {n=albums.length-1;}
 			moveAlbumAnimClass.forCurrent="moveRightCurrentAlbum";
 			moveAlbumAnimClass.forNext="moveRightPreviousAlbum";
+			albumNameAnimClass="albumNameFromTop";
 		}
 	
 	if(n!=albumIndex) {
@@ -54,7 +70,7 @@ function moveAlbum(n) {
 		next.classList.add(moveAlbumAnimClass.forNext);
 		albumIndex = n;
 		albumName.style.display="none";
-		albumName.className = "albumName ";
+		albumName.className = "albumName";
 		albumName.innerText = albums[n].querySelector(".albumName").innerText;
 		albumName.style.display = "block";	
 	}
@@ -65,7 +81,7 @@ var timer = null;
 function setTimer() {
 	timer = setInterval(function() {
 		plusAlbums(1);
-	},2000); 
+	},1500); 
 }
 
 setTimer();
